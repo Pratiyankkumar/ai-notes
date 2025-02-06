cd27640f80e27ac7ace87e19b72a0d5b696891ec
Pratiyank Kumar <159152851+Pratiyankkumar@users.noreply.github.com>
2025-02-06 16:04:24 +0000

    Configurated code sandbox

--- apps/web/api/axiosInstance.ts
+++ apps/web/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://k3z9zp-3000.csb.app",
   headers: {
     "Content-Type": "application/json",
   },
