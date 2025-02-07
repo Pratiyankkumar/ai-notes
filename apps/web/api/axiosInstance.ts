522df23fc6de45600d752bfa7467e2b3ae72faec
Pratiyank <pratiyank91@gmail.com>
2025-02-07 02:19:57 +0000

    Changes urls

--- apps/web/api/axiosInstance.ts
+++ apps/web/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://k3z9zp-3000.csb.app",
+  baseURL: "https://xqyndr-3000.csb.app",
   headers: {
     "Content-Type": "application/json",
   },
