281a5129d767bb9a08aa9c791cbda9589a72cf07
Pratiyank <pratiyank91@gmail.com>
2025-02-07 02:51:56 +0000

    Changed frontend and backend url

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: "https://xqyndr-4000.csb.app" }));
 
 connectDB();
 
