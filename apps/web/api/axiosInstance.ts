178d72fcbc512787a7fefe57e1c6eea6528288a7
Pratiyank <pratiyank91@gmail.com>
2025-02-07 02:30:56 +0000

    changes the urls

--- apps/web/api/axiosInstance.ts
+++ apps/web/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://xqyndr-3000.csb.app",
+  baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
